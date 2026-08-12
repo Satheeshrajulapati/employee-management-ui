d40acbba00f33feb83dc4500b51ba26883e58c83
Satheesh Rajulapati <satheesh.rajulapati@gmail.com>
2026-08-12 23:47:23 +0530

    Folder structure initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [LayoutComponent],
+  template: `
+    <app-layout></app-layout>
+  `
 })
 export class AppComponent {
-  title = 'employee-management-ui';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component')
+        .then(m => m.DashboardComponent)
+  },
+  {
+    path: 'employees',
+    loadComponent: () =>
+      import('./features/employees/components/employee-list/employee-list.component')
+        .then(m => m.EmployeeListComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
